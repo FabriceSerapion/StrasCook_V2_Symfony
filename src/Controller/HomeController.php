b6ae943a3dbf3f3684632a0c96c6c6b9f858d68e
FABRICE SERAPION <serapion.fabrice@hotmail.fr>
2022-11-29 22:47:48 +0100

    adding and configuring user registration and authentication, adding the forms needed to do those tasks, configuring the security.yaml and adding the username and logout link in navbar if registration or authentication is successful

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,13 +10,6 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    // public function index(): Response
-    // {
-    //     return $this->render('home/index.html.twig', [
-    //         'controller_name' => 'HomeController',
-    //     ]);
-    // }
-    // * ? Rewrite of the function for home index to include Menu. Is this the good method ?
     public function index(MenuRepository $menuRepository): Response
     {
         $menus = $menuRepository->findBy(array(), null, 3, 0);
