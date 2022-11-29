b988e22c9ab79f2c316e59a488bff6612a65c122
Iancek <arthuriancek@orange.fr>
2022-11-29 16:00:55 +0100

    menu modal are now filled by their tags + search by tag is done

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\MenuRepository;
+use App\Repository\TagRepository;
+use Doctrine\ORM\Mapping\Id;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,9 +19,13 @@ class HomeController extends AbstractController
     //     ]);
     // }
     // * ? Rewrite of the function for home index to include Menu. Is this the good method ?
-    public function index(MenuRepository $menuRepository): Response
+    public function index(MenuRepository $menuRepository, TagRepository $tagRepository): Response
     {
         $menus = $menuRepository->findBy(array(), null, 3, 0);
+        foreach ($menus as $idx => $menu) {
+            $tagsFromMenu = $tagRepository->findTagsFromMenu($menu->getId());
+            $menus[$idx]->addTag($tagsFromMenu);
+        }
         return $this->render('home/index.html.twig', [
             'menus' => $menus,
         ]);
